4e847716b4e97eb709d34430042583c0c90e4c8c
Jeremiah Brown <jeremiahgoldwin@gmail.com>
2018-11-19 20:54:18 -0600

    starting over

--- new-portfolio/src/App.tsx
+++ new-portfolio/src/App.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
-import styles from './App.block.css';
+import './App.css';
 
 import logo from './logo.svg';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className={styles}>
+      <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
